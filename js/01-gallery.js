1433c329995847b49e0cef8ea638085bad694736
Andrzej Markiewicz <endercom502@gmail.com>
2022-07-24 22:19:43 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,10 +1,10 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-galleryItems.forEach(array => {
+
     const ulId = document.querySelector(".gallery");
-     const arrayLink = `${galleryItems.map((item) => `<li><img src=${item.preview} alt=${item.description}/></li>`)}`;
+     const arrayLink = `${galleryItems.map((item) => `<div><img src=${item.preview} alt=${item.description}/></div>`)}`;
        let sum = 0;
        sum += ulId.insertAdjacentHTML("afterbegin", arrayLink);
          
-   });
+   
 console.log(galleryItems);
